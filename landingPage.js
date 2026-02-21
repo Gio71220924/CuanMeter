155ed424bfd0f583804adb52e45aa70eae37e786
Giovanka Steviano Harry Premono <giovanka.steviano@ti.ukdw.ac.id>
2026-02-22 02:43:49 +0700

    Fix: Fix UI and Logic

--- landingPage.js
+++ landingPage.js
@@ -56,6 +56,10 @@ function createMobileMenu() {
             <span class="material-symbols-outlined">savings</span>
             Simulasi Profit
           </a>
+          <a class="mobile-menu-link" href="PositionSize.html">
+            <span class="material-symbols-outlined">security</span>
+            Kalkulator Amunisi
+          </a>
           <a class="mobile-menu-link" href="DividendYield.html">
             <span class="material-symbols-outlined">payments</span>
             Kalkulator Dividen
