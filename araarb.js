62661c512c1e875d90ec44a69d8e4d5e7077eafa
Giovanka Steviano Harry Premono <giovanka.steviano@ti.ukdw.ac.id>
2026-02-22 13:42:54 +0700

    Fix: FIX UI

--- araarb.js
+++ araarb.js
@@ -31,24 +31,6 @@
   const { formatters, idx, storage } = window.CuanMeterUtils;
   const HISTORY_KEY = 'araarbHistory';
 
-  const formatInput = (input) => {
-    if (!input) return;
-    const selectionStart = input.selectionStart;
-    const oldLength = input.value.length;
-
-    let value = input.value.replace(/[^0-9]/g, "");
-    if (value === "") {
-      input.value = "";
-      return;
-    }
-    
-    input.value = formatters.nf0.format(parseInt(value));
-
-    const newLength = input.value.length;
-    const newPosition = selectionStart + (newLength - oldLength);
-    input.setSelectionRange(newPosition, newPosition);
-  };
-
   const formatTick = (tick) => `Rp ${formatters.nf0.format(tick)}`;
 
   const getFormData = () => {
@@ -181,7 +163,7 @@
 
   const bindEvents = () => {
     els.prevClose?.addEventListener("input", (e) => {
-      formatInput(e.target);
+      formatters.formatInput(e.target);
       render();
     });
     els.prevClose?.addEventListener("change", render);
